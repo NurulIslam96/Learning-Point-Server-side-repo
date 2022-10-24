e8de700d62a3b2d8592edc53c9647785b981ddd3
Nurul Islam <nurul.islam96@aol.com>
2022-10-25 03:59:35 +0600

    functionality added

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json');
+const languages = require('./data/languages.json');
+
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
-})
+    res.send('Server is running');
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = languages.find(language => language.id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
-    console.log('Server is running through port: ', port)
-})
+    console.log('Server is running through port: ', port);
+});
